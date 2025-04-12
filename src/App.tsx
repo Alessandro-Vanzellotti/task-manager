9122cb7d1f839ba24202dd478df14959588961ac
Alessandro Monteiro <alessandro.o.v.monteiro@gmail.com>
2025-04-12 20:11:30 -0300

    implementei a função addNewTask e handleChange

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,31 @@
-import { useState } from 'react'
+import { ChangeEvent, useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
 
   const [task, setTask] = useState("");
-  const [taskList, setTaskList] = useState([]);
+  const [taskList, setTaskList] = useState<string[]>([]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    event.preventDefault();
+    setTask(event.target.value);
+  }
+
+  const addNewTask = () => {
+    setTaskList([...taskList, task]);
+  }
+
+  useEffect(() => {
+    console.log(taskList);
+  },[taskList])
   
 
   return (
     <>
       <header>
         <h2>Task manager:</h2>
-        <input type="text" name="newTask" value={task} placeholder='Type your task' id="" />
-        <input type="button" value="Add" />
+        <input type="text" onChange={handleChange} name="newTask" value={task} placeholder='Type your task' id="" />
+        <input type="button" onClick={addNewTask} value="Add" />
       </header>
 
       <main>
