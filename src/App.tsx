b47d73810e0159ad0d3e61a66f79608cda9fbb85
Alessandro Monteiro <alessandro.o.v.monteiro@gmail.com>
2025-04-13 19:45:52 -0300

    criei uma interface Task; exibi a taskList dentro da tag main

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
-import { ChangeEvent, useEffect, useState } from 'react'
-import './App.css'
+import { ChangeEvent, useEffect, useState } from 'react';
+import './App.css';
+import { Task } from './interfaces/Task';
 
 function App() {
 
-  const [task, setTask] = useState("");
-  const [taskList, setTaskList] = useState<string[]>([]);
+  const [task, setTask] = useState<Task>({title: "", id: 0});
+  const [taskList, setTaskList] = useState<Task[]>([]);
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
     event.preventDefault();
-    setTask(event.target.value);
+    setTask({title: event.target.value, id: taskList.length + 1});
   }
 
   const addNewTask = () => {
     setTaskList([...taskList, task]);
+    setTask({title: "", id: 0});
   }
 
   useEffect(() => {
@@ -24,12 +26,16 @@ function App() {
     <>
       <header>
         <h2>Task manager:</h2>
-        <input type="text" onChange={handleChange} name="newTask" value={task} placeholder='Type your task' id="" />
+        <input type="text" onChange={handleChange} name="newTask" value={task.title} placeholder='Type your task' id="" />
         <input type="button" onClick={addNewTask} value="Add" />
       </header>
 
       <main>
-        
+        {taskList.map((item: Task) => {
+          return (
+            <p>{item.title}</p>
+          )
+        })}
       </main>
     </>
   )
