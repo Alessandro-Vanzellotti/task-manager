60a754a13f8284adc696b51c9a039c39378712e5
Alessandro Monteiro <alessandro.o.v.monteiro@gmail.com>
2025-04-13 20:10:47 -0300

    refatorei a interface Task e implementei a função removeTask

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,30 @@ import { Task } from './interfaces/Task';
 
 function App() {
 
-  const [task, setTask] = useState<Task>({title: "", id: 0});
+  const [task, setTask] = useState<Task>({title: ""});
   const [taskList, setTaskList] = useState<Task[]>([]);
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
     event.preventDefault();
-    setTask({title: event.target.value, id: taskList.length + 1});
+    setTask({title: event.target.value});
   }
 
   const addNewTask = () => {
     setTaskList([...taskList, task]);
-    setTask({title: "", id: 0});
+    setTask({title: ""});
   }
 
+  const removeTask = (itemToRemove: Task) => {
+    let updatedTaskList = taskList.filter(item => item.title != itemToRemove.title);
+    //console.log("removeTask aqui");
+    setTaskList(updatedTaskList);
+  }
+
+  /* const addNewTask = () => {
+    setTaskList([...taskList, task]);
+    setTask({title: "", id: 0});
+  }
+ */
   useEffect(() => {
     console.log(taskList);
   },[taskList])
@@ -33,7 +44,10 @@ function App() {
       <main>
         {taskList.map((item: Task) => {
           return (
-            <p>{item.title}</p>
+            <div>
+              <p>{item.title}</p>
+              <button onClick={() => removeTask(item)}>Delete</button>
+            </div>
           )
         })}
       </main>
--- src/interfaces/Task.ts
+++ src/interfaces/Task.ts
@@ -1,4 +1,3 @@
 export interface Task {
-    title: string,
-    id: number
+    title: string
 }
